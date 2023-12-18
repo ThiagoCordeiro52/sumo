38c198b1678929a37d6376d1b0fec6941b770f7d
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2023-12-18 20:51:06 -0300

    feat: Ajusta layout da tela de home

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,25 +1,37 @@
 import CheckIcon from "@mui/icons-material/Check";
+import { Button } from "@mui/material";
 
 export function Home() {
   return (
-    <div className="bg-zinc-900 text-white text-center h-screen w-screen  flex justify-center items-center flex-col">
-      <h1 className="mb-6">
+    <div className="bg-zinc-800 text-white text-center h-screen w-screen  flex justify-center items-center flex-col">
+      <h1 className="mb-6 text-2xl font-bold text-slate-400">
         Desvende o potencial das ações - seu próximo grande investimento pode
         estar a um código de distância!
       </h1>
       <h2 className="mb-6">
         Descubra se é hora de investir - insira o código da ação:
       </h2>
-      <form className="flex justify-center align-middle space-x-2">
-        <input
-          className="bg-zinc-800 text-zinc-400 p-2 rounded-lg mr outline-none focus:outline-none"
-          type="text"
-          placeholder="MGLU3, PETR4, CPLE6..."
-        ></input>
+      <form className="flex justify-center items-center space-x-4">
+        <div className="w-72">
+          <input
+            className="bg-zinc-700 p-3 rounded-lg outline-none focus:outline-none w-full placeholder-slate-400"
+            type="text"
+            placeholder="MGLU3, PETR4, CPLE6..."
+          />
+        </div>
 
-        <button className="bg-green-600 hover:bg-green-500 p-2 rounded-lg">
-          <CheckIcon /> Confirmar
-        </button>
+        <div className="rounded-lg w-fit h-4/5">
+          <Button
+            variant="contained"
+            color="success"
+            className="h-full"
+            type="submit"
+          >
+            <span className="flex items-center">
+              <CheckIcon /> <span>Confirmar</span>
+            </span>
+          </Button>
+        </div>
       </form>
     </div>
   );
