e1274274e1d7bc7d37ef7e184766c7ccdfc2c918
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2023-12-19 12:29:12 -0300

    feat: Troca da logo e ajustes no layout da pagina

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import CheckIcon from "@mui/icons-material/Check";
 import CircularProgress from "@mui/material/CircularProgress";
-import { Button } from "@mui/material";
+import { Button, Skeleton, Tooltip } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { api } from "../../services/api/url";
@@ -45,7 +45,7 @@ export function Home() {
       >
         <div className="w-72">
           <input
-            className="bg-zinc-700 p-3 rounded-lg outline-none focus:outline-none w-full placeholder-slate-400"
+            className="bg-zinc-600 p-3 rounded-lg outline-none focus:outline-none w-full placeholder-slate-400"
             type="text"
             required
             placeholder="MGLU3, PETR4, CPLE6..."
@@ -53,17 +53,20 @@ export function Home() {
           />
         </div>
 
-        <div className="rounded-lg w-fit h-full">
+        <div className="rounded-lg w-40 h-full">
           <Button
             variant="contained"
             color="success"
-            className="h-full"
+            className="h-full w-full"
             type="submit"
             disabled={isLoading}
           >
-            <span className="flex items-center">
-              <CheckIcon /> <span>Confirmar</span>
-            </span>
+            {isLoading && <CircularProgress color="success" />}
+            {!isLoading && (
+              <span className="flex items-center">
+                <CheckIcon /> <span>Confirmar</span>
+              </span>
+            )}
           </Button>
         </div>
       </form>
@@ -73,58 +76,182 @@ export function Home() {
   const statisticsTable = () => {
     return (
       <div className="w-[90%]">
-        <table className="text-white table-auto border-2 border-collapse border-slate-600">
+        <table className="text-white table-auto border-4 border-collapse border-slate-600">
           <thead className="text-gray-300">
             <tr>
-              <th className="border border-slate-500" colSpan={7}>
-                Indicador
+              <th className="border-2 border-slate-500 p-3" colSpan={7}>
+                <span> Indicador </span>
               </th>
-              <th className="border border-slate-500" colSpan={6}>
+              <th className="border-2 border-slate-500 p-3" colSpan={6}>
                 Análises
               </th>
             </tr>
             <tr>
-              <th className="border border-slate-500 p-2">Valor atual</th>
-              <th className="border border-slate-500 p-2">Valor da empresa</th>
-              <th className="border border-slate-500 p-2">
-                LPA (Lucro por ação)
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Cotação atual da ação">
+                  <span>Valor atual</span>
+                </Tooltip>
               </th>
-              <th className="border border-slate-500 p-2">Beta</th>
-              <th className="border border-slate-500 p-2">
-                PSR (Preço sobre vendas)
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Valor total da empresa">
+                  <span>Valor da empresa</span>
+                </Tooltip>
               </th>
-              <th className="border border-slate-500 p-2">
-                DY (Dividend Yield)
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Lucro por ação">
+                  <span>LPA</span>
+                </Tooltip>
               </th>
-              <th className="border border-slate-500 p-2">
-                P/L (Preço sobre lucro)
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Beta da ação: Indice de verificação da variação do preço esperada pelo mercado">
+                  <span>Beta</span>
+                </Tooltip>
               </th>
-              <th className="border border-slate-500 p-2">Bazin</th>
-              <th className="border border-slate-500 p-2">Graham</th>
-              <th className="border border-slate-500 p-2">Graham revisado</th>
-              <th className="border border-slate-500 p-2">Número de Lynch</th>
-              <th className="border border-slate-500 p-2">
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Preço sobre vendas">
+                  <span>PSR</span>
+                </Tooltip>
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Dividend Yield: Percentual de retorno do dividendo">
+                  <span>DY</span>
+                </Tooltip>
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Tooltip title="Preço sobre lucro">
+                  <span>P/L</span>
+                </Tooltip>
+              </th>
+              <th className="border-2 border-slate-500 p-2">Bazin</th>
+              <th className="border-2 border-slate-500 p-2">Graham</th>
+              <th className="border-2 border-slate-500 p-2">Graham revisado</th>
+              <th className="border-2 border-slate-500 p-2">Número de Lynch</th>
+              <th className="border-2 border-slate-500 p-2">
                 Resultado de Lynch
               </th>
-              <th className="border border-slate-500 p-2">Resultado SUMO</th>
+              <th className="border-2 border-slate-500 p-2">Resultado SUMO</th>
             </tr>
           </thead>
 
           <tbody>
             <tr>
-              <td>{stock?.statistics.actual_value}</td>
-              <td>{stock?.statistics.enterprise_value}</td>
-              <td>{stock?.statistics.eps}</td>
-              <td>{stock?.statistics.beta5}</td>
-              <td>{stock?.statistics.price_sales}</td>
-              <td>{stock?.statistics.ta_dividend_yield}</td>
-              <td>{stock?.statistics.trailing_pe}</td>
-              <td>{stock?.bazin.toFixed(2)}</td>
-              <td>{stock?.graham.toFixed(2)}</td>
-              <td>{stock?.graham_revised.toFixed(2)}</td>
-              <td>{stock?.lynch_number.toFixed(2)}</td>
-              <td>{stock?.lynch_result}</td>
-              <td>{stock?.sumo_result}</td>
+              <td className="p-2">${stock?.statistics.actual_value}</td>
+              <td className="p-2">${stock?.statistics.enterprise_value}</td>
+              <td className="p-2">${stock?.statistics.eps}</td>
+              <td className="p-2">{stock?.statistics.beta5}</td>
+              <td className="p-2">${stock?.statistics.price_sales}</td>
+              <td className="p-2">{stock?.statistics.ta_dividend_yield}</td>
+              <td className="p-2">{stock?.statistics.trailing_pe}</td>
+              <td className="p-2">${stock?.bazin.toFixed(2)}</td>
+              <td className="p-2">${stock?.graham.toFixed(2)}</td>
+              <td className="p-2">${stock?.graham_revised.toFixed(2)}</td>
+              <td className="p-2">{stock?.lynch_number.toFixed(2)}</td>
+              <td className="p-2">{stock?.lynch_result}</td>
+              <td className="p-2">{stock?.sumo_result}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  const renderSkeletonsTable = () => {
+    return (
+      <div className="w-[90%]">
+        <table className="text-white table-auto border-4 border-collapse border-slate-600">
+          <thead className="text-gray-300 border-slate-600">
+            <tr>
+              <th className="border-2 border-slate-500 p-3" colSpan={7}>
+                <Skeleton variant="rounded" width="100%" />
+              </th>
+              <th className="border-2 border-slate-500 p-3" colSpan={6}>
+                <Skeleton variant="rounded" width="100%" />
+              </th>
+            </tr>
+            <tr>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+              <th className="border-2 border-slate-500 p-2">
+                <Skeleton variant="rounded" width="80px" />
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
+              <td className="p-2">
+                <Skeleton variant="rounded" width="100%" />
+              </td>
             </tr>
           </tbody>
         </table>
@@ -138,15 +265,15 @@ export function Home() {
   }, [search]);
 
   return (
-    <div className="bg-zinc-800 text-white text-center h-screen w-screen  flex justify-center items-center flex-col">
+    <div className="bg-neutral-700 text-white text-center h-screen w-screen  flex justify-center items-center flex-col">
       <img src={Logo} width={"20%"} />
       <Header />
       {searchStock()}
       <span className="text-lg mt-6 mb-3 font-semibold font-mono">
         {stock?.statistics.name}
       </span>
-      {isLoading && <CircularProgress color="success" />}
-      {!isLoading && statisticsTable()}
+      {isLoading && renderSkeletonsTable()}
+      {!isLoading && stock && statisticsTable()}
     </div>
   );
 }
